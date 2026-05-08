b0ed1db352887b151663c56b6697f90b63ab7639
Mayur Bhardwaj <91755325+Mayur-Bhardwaj@users.noreply.github.com>
2026-05-08 08:23:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function insert_Row() {
     //Write your code here
-  
+
+  const table = document.getElementById("sampleTable");
+
+  // Insert new row at the top
+  const newRow = table.insertRow(0);
+
+  // Insert cells
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+
+  // Add text
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
+
   
 }
